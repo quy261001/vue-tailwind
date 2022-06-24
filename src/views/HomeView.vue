9edb15488f3aaccefe4c2ae9f448790a0cf22c57
your name <abc@gmail.com>
2022-06-24 10:32:41 +0700

    e

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h1 class="mt-[20px]"> day la h1</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import '../assets/tailwind.css'
 export default {
   name: 'HomeView',
   components: {
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
